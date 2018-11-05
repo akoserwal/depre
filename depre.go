54e61b3a72a08785ba03c4ebeeb34bd8bf958559
akoserwal <akoserwa@redhat.com>
2018-11-06 00:27:37 +0530

    refactored into modular functions

--- depre.go
+++ depre.go
@@ -9,7 +9,18 @@ import (
 	"strings"
 )
 
+//COMPILE scope
+const COMPILE = "compile"
+
+//TEST scope
+const TEST = "test"
+
+//INFO build type
+const INFO = "INFO"
+
 func main() {
+
+	const regexPattern = "[^a-zA-Z0-9.:-]"
 
 	scanner := bufio.NewScanner(os.Stdin)
 	checkError(scanner.Err())
@@ -19,29 +30,42 @@ func main() {
 
 		text := scanner.Text()
 
-		reg, err := regexp.Compile("[^a-zA-Z0-9.:-]")
+		reg, err := regexp.Compile(regexPattern)
 		checkError(err)
 
 		processedString := reg.ReplaceAllString(text, "")
-		deps := strings.Split(processedString, "INFO")
+		deps := strings.Split(processedString, INFO)
 
-		for i := range deps {
-			if strings.Contains(deps[i], "compile") || strings.Contains(deps[i], "test") {
-				value, ok := depMap[deps[i]]
-				if ok == true {
-					depMap[deps[i]] = value + 1
-				} else {
-					depMap[deps[i]] = 1
+		countDependencyOccurance(deps, depMap)
+		checkError(err)
 
-				}
+	}
+
+	iterateMapAndPrint(depMap)
+
+}
+
+func iterateMapAndPrint(m map[string]int) {
+	for k, v := range m {
+		fmt.Printf("[%s] occurance:[%d]\n", k, v)
+
+	}
+}
+
+func countDependencyOccurance(deps []string, depMap map[string]int) map[string]int {
+
+	for i := range deps {
+		if strings.Contains(deps[i], COMPILE) || strings.Contains(deps[i], TEST) {
+			value, ok := depMap[deps[i]]
+			if ok == true {
+				depMap[deps[i]] = value + 1
+			} else {
+				depMap[deps[i]] = 1
+
 			}
 		}
 	}
-
-	for k, v := range depMap {
-		fmt.Printf("dependency:[%s] occurance:[%d]\n", k, v)
-	}
-
+	return depMap
 }
 
 func checkError(err error) {
